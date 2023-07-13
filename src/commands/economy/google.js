2052db2e44cb8611165d7b0770ae606b256d88b7
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-07-13 13:50:33 -0400

    eeeee

---
+++ src/commands/economy/google.js
@@ -0,0 +1,43 @@
+module.exports = {
+  name: 'google',
+  description: 'Perform a Google search and display the top search results!',
+  options: [
+    {
+      name: 'query',
+      description: 'The search query',
+      type: 3, // Corrected type value for string option
+      required: true,
+    },
+  ],
+
+  callback: (client, interaction) => {
+    const query = interaction.options.getString('query');
+
+    const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+
+    const embed = {
+      title: 'Google Search Results',
+      description: `Click here to view the search results for "${query}":\n[Google Search Results](${searchUrl})`,
+      color: 0x4285F4, // Google Blue color
+      thumbnail: {
+        url: 'https://i.imgur.com/CQiKstK.jpg',
+      },
+      footer: {
+        text: 'DolphinNotBot - Created by DolphinNotFound',
+        icon_url: 'https://i.imgur.com/CQiKstK.jpg',
+      },
+      timestamp: new Date(),
+    };
+
+    interaction.reply({ embeds: [embed] });
+  },
+};
+
+
+function formatUptime(uptime) {
+  const totalSeconds = Math.floor(uptime / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+  return `${hours}h ${minutes}m ${seconds}s`;
+}
---
+++ src/commands/misc/say.js
@@ -0,0 +1,42 @@
+module.exports = {
+    name: 'say',
+    description: 'Make the bot say a message!',
+    options: [
+      {
+        name: 'message',
+        description: 'The message to be said',
+        type: 3, // Updated type value for string option
+        required: true,
+      },
+    ],
+  
+    callback: (client, interaction) => {
+      const message = interaction.options.getString('message');
+  
+      const embed = {
+        title: 'Said by DolphinNotBot',
+        description: message,
+        color: 0x964B00,
+        thumbnail: {
+            url: 'https://i.imgur.com/CQiKstK.jpg',
+          },
+          footer: {
+            text: 'DolphinNotBot - Created by DolphinNotFound',
+            icon_url: 'https://i.imgur.com/CQiKstK.jpg',
+          },
+          timestamp: new Date(),
+      };
+  
+      // Sending the embed as a reply
+      interaction.reply({ embeds: [embed] });
+    },
+  };
+
+
+  function formatUptime(uptime) {
+    const totalSeconds = Math.floor(uptime / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+    return `${hours}h ${minutes}m ${seconds}s`;
+  }
