c379e512f4482b6774c3bd02fd36d4bddf0a4f37
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-07-15 19:53:31 -0400

    e

---
+++ src/commands/economy/timer.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: 'timer',
+    description: 'Start a timer',
+    testOnly: true,
+    options: [
+      {
+        name: 'duration',
+        description: 'Duration of the timer in minutes',
+        type: 4,
+        required: true,
+      },
+    ],
+    callback: async (client, interaction) => {
+      const durationInMinutes = interaction.options.getInteger('duration');
+      const durationInSeconds = durationInMinutes * 60;
+  
+      await interaction.reply(`Timer started: ${durationInMinutes} minute(s)`);
+      let remainingTime = durationInSeconds;
+  
+      const intervalId = setInterval(async () => {
+        remainingTime--;
+  
+        if (remainingTime > 0) {
+          const remainingMinutes = Math.floor(remainingTime / 60);
+          const remainingSeconds = remainingTime % 60;
+          const formattedTime = `${remainingMinutes} minute(s) ${remainingSeconds} second(s)`;
+          await interaction.editReply(`Timer: ${formattedTime}`);
+        } else {
+          clearInterval(intervalId);
+          await interaction.editReply('Timer completed!');
+        }
+      }, 1000);
+    },
+  };
+  
