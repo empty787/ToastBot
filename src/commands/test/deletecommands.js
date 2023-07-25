734446f7010301ac7eeb34f46a28839a320b280c
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-07-25 13:19:14 -0400

    a

---
+++ src/commands/test/deletecommands.js
@@ -0,0 +1,15 @@
+// used for deleting old commands just paste your old commands here and put deleted: true,
+// example lol
+
+// module.exports = {
+//  name: 'examplecommand',
+//  description: 'susssy',
+//  deleted: true,
+
+//  callback: (client, interaction) => {
+//    interaction.reply(`this is a test lol`);
+//  },
+// };
+
+// put your code below
+
