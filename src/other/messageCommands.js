72f6d785718eadc27782fd749fc6156bea36fb40
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-07-26 13:22:57 -0400

    a

--- src/other/messageCommands.js
+++ src/other/messageCommands.js
@@ -7,6 +7,28 @@ module.exports = {
       if (message.content === '!ping') {
         message.reply('Pong! 🏓');
       }
+
+      if (message.content === '!date') {
+        const currentDate = new Date().toLocaleDateString();
+        message.reply(`Today's date is ${currentDate}`);
+      }
+      
+      if (message.content === '!roll') {
+        const randomNum = Math.floor(Math.random() * 6) + 1;
+        message.reply(`You rolled a ${randomNum}`);
+      }   
+      
+      const catFacts = [
+        "Cats sleep for about 70% of their lives.",
+        "A group of cats is called a 'clowder'.",
+        "Cats have whiskers on the backs of their front legs too.",
+        "Cats have a special reflective layer behind their retinas, which helps them see better in low light.",
+      ];
+      
+      if (message.content === '!catfact') {
+        const randomFact = catFacts[Math.floor(Math.random() * catFacts.length)];
+        message.reply(`Here's a cat fact: ${randomFact}`);
+      }  
   
       if (message.mentions.users.has(client.user.id)) {
         const content = message.content.replace(`<@!${client.user.id}>`, '').trim();
